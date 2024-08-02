371da6ff9a3273adaeef83d89ac23bf62e004083
chengxing <zcx5613@gmail.com>
2024-08-02 13:51:07 +0900

    'using namespace nvcuda' is changed to 'using namespace nvcuda::wmma'

--- src/kernelFunc.cu
+++ src/kernelFunc.cu
@@ -3,7 +3,7 @@
 #include "matrixSetting.hpp"
 #include "kernelFunc.cuh"
 
-using namespace nvcuda;
+using namespace nvcuda::wmma;
 
 __global__ void convertFp32ToFp16(const int n, const float *in, half *out) {
     int idx = (int) (blockDim.x * blockIdx.x + threadIdx.x);
@@ -53,13 +53,13 @@ __global__ void wmmaExample1DGrid(const int M, const int N, const int K,
         cCol = warpId * WMMA_M / M * WMMA_N;
     }
 
-    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> aFrag;
-    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> bFrag;
+    fragment<matrix_a, WMMA_M, WMMA_N, WMMA_K, half, row_major> aFrag;
+    fragment<matrix_b, WMMA_M, WMMA_N, WMMA_K, half, row_major> bFrag;
 
-    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> accFrag; // Fragment accumulators
-    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> cFrag;
+    fragment<accumulator, WMMA_M, WMMA_N, WMMA_K, float> accFrag; // Fragment accumulators
+    fragment<accumulator, WMMA_M, WMMA_N, WMMA_K, float> cFrag;
 
-    wmma::fill_fragment(accFrag, 0.0f);
+    fill_fragment(accFrag, 0.0f);
 
     // Leading dimensions. Packed with no transpositions.
     int lda = K;
@@ -78,10 +78,10 @@ __global__ void wmmaExample1DGrid(const int M, const int N, const int K,
 
         // Bounds checking
         if (aRow < M && aCol < K && bRow < N && bCol < K) {
-            wmma::load_matrix_sync(aFrag, aTileOffsetPrt, lda);
-            wmma::load_matrix_sync(bFrag, bTileOffsetPrt, ldb);
+            load_matrix_sync(aFrag, aTileOffsetPrt, lda);
+            load_matrix_sync(bFrag, bTileOffsetPrt, ldb);
 
-            wmma::mma_sync(accFrag, aFrag, bFrag, accFrag);
+            mma_sync(accFrag, aFrag, bFrag, accFrag);
         }
     }
 
@@ -89,14 +89,14 @@ __global__ void wmmaExample1DGrid(const int M, const int N, const int K,
 
     // Bounds checking
     if (cRow < M && cCol < N) {
-        wmma::load_matrix_sync(cFrag, cTileOffsetPrt, ldc, wmma::mem_row_major);
+        load_matrix_sync(cFrag, cTileOffsetPrt, ldc, mem_row_major);
 
 #pragma unroll
         for (int idx = 0; idx < cFrag.num_elements; ++idx) {
             cFrag.x[idx] = alpha * accFrag.x[idx] + beta * cFrag.x[idx];
         }
 
-        wmma::store_matrix_sync(cTileOffsetPrt, cFrag, ldc, wmma::mem_row_major);
+        store_matrix_sync(cTileOffsetPrt, cFrag, ldc, mem_row_major);
     }
 
 }
@@ -114,13 +114,13 @@ __global__ void wmmaExample2DGrid(const int M, const int N, const int K,
         return;
     }
 
-    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> aFrag;
-    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> bFrag;
+    fragment<matrix_a, WMMA_M, WMMA_N, WMMA_K, half, row_major> aFrag;
+    fragment<matrix_b, WMMA_M, WMMA_N, WMMA_K, half, row_major> bFrag;
 
-    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> accFrag;
-    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> cFrag;
+    fragment<accumulator, WMMA_M, WMMA_N, WMMA_K, float> accFrag;
+    fragment<accumulator, WMMA_M, WMMA_N, WMMA_K, float> cFrag;
 
-    wmma::fill_fragment(accFrag, 0.0f);
+    fill_fragment(accFrag, 0.0f);
 
     const int lda = K;
     const int ldb = N;
@@ -137,22 +137,22 @@ __global__ void wmmaExample2DGrid(const int M, const int N, const int K,
             const auto aOffsetPtr = mtrA + aRow * lda + aCol;
             const auto bOffsetPtr = mtrB + bRow * ldb + bCol;
 
-            wmma::load_matrix_sync(aFrag, aOffsetPtr, lda);
-            wmma::load_matrix_sync(bFrag, bOffsetPtr, ldb);
+            load_matrix_sync(aFrag, aOffsetPtr, lda);
+            load_matrix_sync(bFrag, bOffsetPtr, ldb);
 
-            wmma::mma_sync(accFrag, aFrag, bFrag, accFrag);
+            mma_sync(accFrag, aFrag, bFrag, accFrag);
         }
     }
 
     const auto cOffsetPtr = mtrC + cRow * ldc + cCol;
-    wmma::load_matrix_sync(cFrag, cOffsetPtr, ldc, wmma::mem_row_major);
+    load_matrix_sync(cFrag, cOffsetPtr, ldc, mem_row_major);
 
 #pragma unroll
     for (int idx = 0; idx < cFrag.num_elements; ++idx) {
         cFrag.x[idx] = alpha * accFrag.x[idx] + beta * cFrag.x[idx];
     }
 
-    wmma::store_matrix_sync(cOffsetPtr, cFrag, ldc, wmma::mem_row_major);
+    store_matrix_sync(cOffsetPtr, cFrag, ldc, mem_row_major);
 }
 
 __global__ void wmmaExample2DGrid2(const int M, const int N, const int K,
@@ -168,13 +168,13 @@ __global__ void wmmaExample2DGrid2(const int M, const int N, const int K,
         return;
     }
 
-    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> aFrag;
-    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> bFrag;
+    fragment<matrix_a, WMMA_M, WMMA_N, WMMA_K, half, row_major> aFrag;
+    fragment<matrix_b, WMMA_M, WMMA_N, WMMA_K, half, col_major> bFrag;
 
-    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> accFrag;
-    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> cFrag;
+    fragment<accumulator, WMMA_M, WMMA_N, WMMA_K, float> accFrag;
+    fragment<accumulator, WMMA_M, WMMA_N, WMMA_K, float> cFrag;
 
-    wmma::fill_fragment(accFrag, 0.0f);
+    fill_fragment(accFrag, 0.0f);
 
     const int lda = K;
     const int ldb = K;
@@ -191,15 +191,15 @@ __global__ void wmmaExample2DGrid2(const int M, const int N, const int K,
             const auto aOffsetPtr = mtrA + aRow * lda + aCol;
             const auto bOffsetPtr = mtrB + bRow * ldb + bCol;
 
-            wmma::load_matrix_sync(aFrag, aOffsetPtr, lda);
-            wmma::load_matrix_sync(bFrag, bOffsetPtr, ldb);
+            load_matrix_sync(aFrag, aOffsetPtr, lda);
+            load_matrix_sync(bFrag, bOffsetPtr, ldb);
 
-            wmma::mma_sync(accFrag, aFrag, bFrag, accFrag);
+            mma_sync(accFrag, aFrag, bFrag, accFrag);
         }
     }
 
     const auto cOffsetPtr = mtrC + cRow * ldc + cCol;
-    wmma::load_matrix_sync(cFrag, cOffsetPtr, ldc, wmma::mem_col_major);
+    load_matrix_sync(cFrag, cOffsetPtr, ldc, mem_col_major);
 
 #pragma unroll
     for (int idx = 0; idx < cFrag.num_elements; ++idx) {
@@ -207,7 +207,7 @@ __global__ void wmmaExample2DGrid2(const int M, const int N, const int K,
     }
 
     // Store the output
-    wmma::store_matrix_sync(cOffsetPtr, cFrag, ldc, wmma::mem_col_major);
+    store_matrix_sync(cOffsetPtr, cFrag, ldc, mem_col_major);
 }
 
 __global__ void wmmaExample2DGrid3(const int M, const int N, const int K,
@@ -226,12 +226,12 @@ __global__ void wmmaExample2DGrid3(const int M, const int N, const int K,
     }
 
     // Declare the fragments
-    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> aFrag;
-    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> bFrag;
-    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> accFrag;
-    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> cFrag;
+    fragment<matrix_a, WMMA_M, WMMA_N, WMMA_K, half, col_major> aFrag;
+    fragment<matrix_b, WMMA_M, WMMA_N, WMMA_K, half, col_major> bFrag;
+    fragment<accumulator, WMMA_M, WMMA_N, WMMA_K, float> accFrag;
+    fragment<accumulator, WMMA_M, WMMA_N, WMMA_K, float> cFrag;
 
-    wmma::fill_fragment(accFrag, 0.0f);
+    fill_fragment(accFrag, 0.0f);
 
     // Leading dimensions. Packed with no transpositions.
     const int lda = M;
@@ -252,16 +252,16 @@ __global__ void wmmaExample2DGrid3(const int M, const int N, const int K,
             const auto bOffsetPtr = mtrB + bRow + bCol * ldb;
 
             // Load the inputs
-            wmma::load_matrix_sync(aFrag, aOffsetPtr, lda);
-            wmma::load_matrix_sync(bFrag, bOffsetPtr, ldb);
+            load_matrix_sync(aFrag, aOffsetPtr, lda);
+            load_matrix_sync(bFrag, bOffsetPtr, ldb);
 
             // Perform the matrix multiplication
-            wmma::mma_sync(accFrag, aFrag, bFrag, accFrag);
+            mma_sync(accFrag, aFrag, bFrag, accFrag);
         }
     }
 
     const auto cOffsetPtr = mtrC + cRow + cCol * ldc;
-    wmma::load_matrix_sync(cFrag, cOffsetPtr, ldc, wmma::mem_col_major);
+    load_matrix_sync(cFrag, cOffsetPtr, ldc, mem_col_major);
 
 #pragma unroll
     for (int idx = 0; idx < cFrag.num_elements; ++idx) {
@@ -269,5 +269,5 @@ __global__ void wmmaExample2DGrid3(const int M, const int N, const int K,
     }
 
     // Store the output
-    wmma::store_matrix_sync(cOffsetPtr, cFrag, ldc, wmma::mem_col_major);
+    store_matrix_sync(cOffsetPtr, cFrag, ldc, mem_col_major);
 }
